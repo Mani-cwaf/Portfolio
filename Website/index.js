f14978469e3e0dbd76fbb0303430773555df1272
Mani_Dev <dhingramsp@Gmail.com>
2022-10-28 00:04:45 +0530

    L

---
+++ Website/index.js
@@ -0,0 +1,38 @@
+const menu = document.getElementById("menu");
+var locked = false;
+var lockedindex;
+
+function ClearContent(){
+  Array.from(document.getElementsByClassName("content")).forEach((item) => {
+    item.style.visibility = "hidden";
+  });
+}
+ClearContent();
+
+Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+      ClearContent();
+      document.getElementsByClassName("content")[index].style.visibility = "visible";
+    }
+    item.onmousedown = () => {
+      if (locked) {
+        if (lockedindex == index) {
+          locked = false;
+        } else {
+          lockedindex = menu.dataset.activeIndex;
+        }
+      } else {
+        locked = true;
+        lockedindex = menu.dataset.activeIndex;
+      }
+    }
+    item.onmouseout = () => {
+      if (!locked) {
+        ClearContent();
+      } else {
+        ClearContent();
+        document.getElementsByClassName("content")[lockedindex].style.visibility = "visible";
+      }
+    }
+});
