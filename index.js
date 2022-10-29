aebb210b51953315be2cc1f7a5693c50b4cd280f
Mani_Dev <dhingramsp@Gmail.com>
2022-10-29 11:22:06 +0530

    added css styling to locked tabs

--- index.js
+++ index.js
@@ -2,33 +2,43 @@ const menu = document.getElementById("menu");
 var locked = false;
 var lockedindex;
 
-function ClearContent(){
-  Array.from(document.getElementsByClassName("content")).forEach((item) => {
-    item.style.visibility = "hidden";
-  });
+function ClearContent(lock){
+  if (lock){
+    Array.from(document.getElementsByClassName("menu-item")).forEach((item) => {
+      item.dataset.locked = "false";
+    });
+  } else {
+    Array.from(document.getElementsByClassName("content")).forEach((item) => {
+      item.style.visibility = "hidden";
+    });
+  }
 }
 ClearContent();
 
 Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
     item.onmouseover = () => {
       menu.dataset.activeIndex = index;
-      ClearContent();
+      ClearContent(false);
       document.getElementsByClassName("content")[index].style.visibility = "visible";
     }
     item.onmousedown = () => {
       if (locked) {
         if (lockedindex == index) {
           locked = false;
+          ClearContent(true);
         } else {
+          ClearContent(true);
           lockedindex = menu.dataset.activeIndex;
+          item.dataset.locked = "true"
         }
       } else {
         locked = true;
         lockedindex = menu.dataset.activeIndex;
+        item.dataset.locked = "true"
       }
     }
     item.onmouseout = () => {
-      ClearContent();
+      ClearContent(false);
       if (locked) {
         document.getElementsByClassName("content")[lockedindex].style.visibility = "visible";
         menu.dataset.activeIndex = lockedindex;
