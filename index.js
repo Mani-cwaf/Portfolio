a796872792cd02c7dcb93da282a117e8756280de
Mani_Dev <dhingramsp@Gmail.com>
2022-12-03 21:07:59 +0530

    improved dark mode, added dark mode button changer, and made buttons more responsive

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
-$(document).ready(function () {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry);
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            } else {
-                entry.target.classList.remove('show');
-            }
-        });
-    });
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    $('.toggle-dark-mode').addClass('active')
+    $('.toggle-dark-mode').addClass('bxs-bulb')
+    $('.toggle-dark-mode').removeClass('bx-bulb')
+}
 
-    const hiddenElements = document.querySelectorAll('.hidden');
-    hiddenElements.forEach((el) => observer.observe(el));
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    if (event.matches) {
+        $('.toggle-dark-mode').addClass('active')
+    } else {
+        $('.toggle-dark-mode').removeClass('active')
+    }
+});
 
-    $(window).scroll(function () {
-        scroll = this.scrollY
-        if (scroll > 5) {
-            $('.l-header').addClass("sticky");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
         } else {
-            $('.l-header').removeClass("sticky");
-        }
-        if ($(window).scrollTop() + $(window).height() > $(document).height()) {
-            $('.footer').addClass("sticky");
-        } else {
-            $('.footer').removeClass("sticky");
+            entry.target.classList.remove('show');
         }
     });
+});
 
-    new Typed(".typing", {
-        strings: ["Web Dev", "Designer", "Game Dev", "Student"], typeSpeed: 80, backSpeed: 60, loop: true
-    });
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
-    $('.nav-list li a').click(function (e) {
-        $('.nav-list li a').removeClass('active');
-        if (!$(this).hasClass('active')) {
-            $(this).addClass('active');
-        }
-    });
-    $('.toggle').click(function (e) {
-        if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
-        } else {
-            $(this).addClass('active');
-        }
-    });
+$(window).scroll(function () {
+    scroll = this.scrollY
+    if (scroll > 5) {
+        $('.l-header').addClass("sticky");
+    } else {
+        $('.l-header').removeClass("sticky");
+    }
+    if ($(window).scrollTop() + $(window).height() > $(document).height()) {
+        $('.footer').addClass("sticky");
+    } else {
+        $('.footer').removeClass("sticky");
+    }
+});
 
+new Typed(".typing", {
+    strings: ["Web Dev", "Designer", "Game Dev", "Student"], typeSpeed: 80, backSpeed: 60, loop: true
+});
+
+$('.nav-list li a').click(function (e) {
+    $('.nav-list li a').removeClass('active');
+    if (!$(this).hasClass('active')) {
+        $(this).addClass('active');
+    }
+});
+$('.nav-toggle').click(function (e) {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).addClass('active');
+    }
+});
+$('.toggle-dark-mode').click(function (e) {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $('.toggle-dark-mode').addClass('bx-bulb')
+        $('.toggle-dark-mode').removeClass('bxs-bulb')
+    } else {
+        $(this).addClass('active');
+        $('.toggle-dark-mode').addClass('bxs-bulb')
+        $('.toggle-dark-mode').removeClass('bx-bulb')
+    }
 });
